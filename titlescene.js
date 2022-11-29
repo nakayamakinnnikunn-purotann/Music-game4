901e8371bd62b36488fe21008204482d1addf22c
nakayamakinnnikunn-purotann <115986363+nakayamakinnnikunn-purotann@users.noreply.github.com>
2022-11-29 16:48:34 +0900

    Update titlescene.js

--- titlescene.js
+++ titlescene.js
@@ -11,7 +11,7 @@ phina.define('TitleScene', {
 
     // タイトルラベル
     Label({
-      text: "Music-Game",
+      text: "Music-Game4",
       fill: "black",
       stroke: "#2F3CEC",
       strokeWidth: 6,
